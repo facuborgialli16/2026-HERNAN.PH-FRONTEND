af182ae7d9f367905a8cec8dc84b89dd64bdb86e
facu.dev <facuborgialli16@gmail.com>
2026-06-19 12:10:26 -0300

    Primer commit

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // The URL of the backend API
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
 
 export const apiClient = axios.create({
     baseURL,
